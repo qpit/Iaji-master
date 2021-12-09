6067ad5e2134db4784bc16450c68e67f5c5374e2
QPIT Login <qpitlab@dtu.dk>
2021-12-09 13:03:09 +0100

    Progress on acquire() function in module AcquisitionSystem of ScissorQKD

--- Iaji/Physics/Experiment/Optics/QKD/QuantumScissorQKD/AcquisitionSystem.py
+++ Iaji/Physics/Experiment/Optics/QKD/QuantumScissorQKD/AcquisitionSystem.py
@@ -10,14 +10,28 @@ import shutil
 from Iaji.InstrumentsControl.LecroyOscilloscope import LecroyOscilloscope
 #%%
 class AcquisitionSystem:
-    def __init__(self, scope_IP_address, channel_names, name="Acquisition System"):
+    def __init__(self, scope_IP_address, channel_names, name="Acquisition System", save_directory=os.getcwd()):
         self.name = name
         self.scope = LecroyOscilloscope(IP_address=scope_IP_address,channel_names=channel_names, connect=True)
+        self.save_directory = save_directory
 
-    def acquire(self, channel_names): #TODO
+    def acquire(self, channel_names, save_directory=None):
+        #Enable the input channels in the scope
+        for name in channel_names:
+            if not self.scope.channels[name].is_enabled():
+                self.scope.channels[name].enable(True)
+        #Start acquisition
         self.scope.start_acquisition(store_all_traces=False)
+        #Store traces locally in the oscilloscope
         for channel_name in channel_names:
-            channel_name
-            pass
+            self.scope.store_trace(channel_name=channel_name)
+        #Transfer the selected traces to the host save directory
+        if save_directory is None:
+            save_directory = self.save_directory
+
+
+
+    def set_save_directory(self, save_directory):
+        self.save_directory = save_directory
 
 
